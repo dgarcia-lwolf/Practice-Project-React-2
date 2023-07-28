785f5ae8f4a169a27f80f10792d96e1a3de5692d
Danilo Garcia <dgarcia@lwolf.com>
2023-07-28 07:54:38 -0500

    latest changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -23,7 +23,7 @@
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
 import HomePage from './pages/Home';
-import EventsPage from './pages/Events';
+import EventsPage, { loader as eventsLoader } from './pages/Events';
 import EventDetailPage from './pages/EventDetail';
 import NewEventPage from './pages/NewEvent';
 import EditEventPage from './pages/EditEvent';
@@ -36,7 +36,11 @@ const router = createBrowserRouter([
     children: [
       { index: true, element: <HomePage/> },
       { path: 'events', element: <EventsRootLayout/>, children: [
-        { index: true, element: <EventsPage/> },
+        { 
+          index: true,
+          element: <EventsPage/>,
+          loader: eventsLoader
+        },
         { path: ':eventId', element: <EventDetailPage/> },
         { path: 'new', element: <NewEventPage/> },
         { path: ':eventId/edit', element: <EditEventPage/> }
